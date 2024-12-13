24f0db486ee1845ea2804599cc228312d20c7c06
firman <firmansyahsitepu700@gmal.com>
2024-12-13 22:57:23 +0700

    database

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\Shop\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ShopServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Logic register
+    }
+
+    public function boot()
+    {
+        // Logic boot
+    }
+}
